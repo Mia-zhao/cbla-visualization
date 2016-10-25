1901ae1dae0028467b098e02c5829e331309286c
Mia Zhao <y4zhao@uwaterloo.ca>
2016-10-24 23:21:48 -0400

    update test program

--- qtgui_test.py
+++ qtgui_test.py
@@ -5,14 +5,19 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 def main():
-    
-    app = QApplication([])
 
+    app = None
+    if (QApplication.instance()):
+        app = QApplication.instance()
+    else:
+        app = QApplication([])
     win = qtgui.VisualApp()
 
     win.showMaximized()  
-    
+
     win.bottom.btn_cancel.clicked.connect(app.quit)
+
+    #in = qtgui.CBLAPlot()
 
     sys.exit(app.exec_())
 
